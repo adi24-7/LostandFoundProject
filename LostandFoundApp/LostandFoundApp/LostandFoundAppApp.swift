2cb4bfd816087110c75fc1f3beed61dfb44b51ea
smithap2 <smitha.pasumarti@gmail.com>
2025-11-03 17:32:36 -0500

    Add Xcode project

---
+++ LostandFoundApp/LostandFoundApp/LostandFoundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LostandFoundAppApp.swift
+//  LostandFoundApp
+//
+//  Created by Smitha Pasumarti on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct LostandFoundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
